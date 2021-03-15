7e6f4e188c9d2bc22402bb379ceed56948a8b7eb
pybusiness <78341941+pybusiness@users.noreply.github.com>
2021-03-15 23:11:56 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FoodExpress</title>
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+    </head>
+    <header>
+        <div class="logo">
+            <img src="images/logo.png" alt="">
+        </div>
+        <div class="search">
+            <input type="text" placeholder="Поиск">
+        </div>
+    </header>
+</html>
