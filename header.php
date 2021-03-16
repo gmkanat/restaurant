dd076a7d640de7c9394bb44e109cffdb7129d0fd
Kanat <kanat_9999@inbox.ru>
2021-03-16 10:45:34 +0600

    header

--- header.php
+++ header.php
@@ -7,13 +7,38 @@
         <link rel="stylesheet" href="css/main.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
     </head>
     <header>
-        <div class="logo">
-            <img src="images/logo.png" alt="">
-        </div>
-        <div class="search">
-            <input type="text" placeholder="Поиск">
+        <div class="header">
+            <div class="logo">
+                <a href="#" class="header_logo">
+                    <img src="images/logo.png" alt="">
+                </a>
+                
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Поиск">
+            </div>
+            <div class="register_form">
+                <div class="register_login">
+                    <div class="btn_login">
+                        Log in
+                    </div>      
+                </div>
+                <div class="register_signup">
+                     <div class="btn_signup">
+                        Sign Up
+                    </div> 
+                </div>
+                <div class="register_lang">
+                    <select name="lang" id="lang">
+                        <option value="eng">English</option>
+                        <option value="rus">Русский</option>
+                    </select>
+                </div>
+            </div>
         </div>
     </header>
 </html>
